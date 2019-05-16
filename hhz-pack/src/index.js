e05942f3a9cefeaaf8c5617aaf21266465eb2f1a
hhz <860601963@qq.com>
2019-05-16 15:26:19 +0800

    get the dependency tree in demo

--- hhz-pack/src/index.js
+++ hhz-pack/src/index.js
@@ -2,6 +2,7 @@
 // 用node解释器解析此文件
 
 const path = require('path')
+const fs = require('fs')
 // 第一步：读取config配置内容
 // 默认配置
 const defaultConfig = {
@@ -19,11 +20,51 @@ class hhzPack{
         // 存储一下配置
         this.config = config
         this.entry = config.entry
-        
+        // 工作 根目录
+        this.root = process.cwd()
+        // 存储所有代码
+        this.modules = {}
+    }
+    // 能够解析文件内容中的require('xx.js')这种格式
+    parse(code, parent){
+        let deps = []
+        let r = /require\((.*)\)/g
+        // require('xx')替换为__hhzpack_require__
+        code = code.replace(r, function(match, arg){
+            // 依赖路径
+            const retPath = path.join(parent, arg.replace(/'|"/g, ''))
+            deps.push(retPath)
+            return `__hhzpack_require__("./${retPath}")`
+        })
+        return { code, deps }
+    }
+    // 传入entry，是因为可以有多个依赖，需要递归调用
+    creatModule(modulePath, name){
+        // 处理循环依赖
+        // if(this.modules[modulePath]) {
+        //     // 出现了循环依赖
+        // }
+        const fileContent = fs.readFileSync(modulePath, 'utf-8')
+        // 替换后的代码的依赖数组
+        const { code, deps } = this.parse(fileContent, path.dirname(name))
+        this.modules[name] = `function(module, exports, __hhzpack_require__){
+            eval(\'${code}\')
+        }
+        `
+        console.log (code)
+        console.log (deps)
+        // 循环获取所有依赖数组的内容
+        deps.forEach(dep => {
+            this.creatModule(path.join(this.root, dep), './'+dep)
+        })
+        // console.log(code)
+        // console.log(name)
     }
     start() {
         console.log('开始解析文件的依赖')
-
+        const entryPath = path.resolve(this.root, this.entry)
+        this.creatModule(entryPath, this.entry)
+        console.log(this.modules)
     }
 }
 
---
+++ webpackxx/src/common/util.js
@@ -0,0 +1,3 @@
+module.exports = (age) => {
+    console.log(`我的年龄是${age}岁`)
+}
